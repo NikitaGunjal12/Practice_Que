e5c7f5036bb4adb1edf28a5dd3e26da710e229fd
NikitaGunjal12 <nikitagunjal.rmdstic.comp@gmail.com>
2024-07-18 22:36:35 +0530

    firstProgram

---
+++ Solution.java
@@ -0,0 +1,27 @@
+/* Minimum Difference Between Largest and Smallest value in Three Move.
+You are given an integer array nums.
+In one move, you can choose one element of nums and change it to any value.
+Return the minimum difference between the largest and smallest value of nums after performing at most three moves.
+Example 1:
+Input: nums = [5,3,2,4]
+Output: 0
+Explanation: We can make at most 3 moves.
+In the first move, change 2 to 3. nums becomes [5,3,3,4].
+In the second move, change 4 to 3. nums becomes [5,3,3,3].
+In the third move, change 5 to 3. nums becomes [3,3,3,3].
+After performing 3 moves, the difference between the minimum and maximum is 3 - 3 = 0 */
+
+package Practice_Que;
+class Solution {
+    public int minDifference(int[] nums) {
+        
+        Arrays.sort(nums);
+        int n = nums.length, min =Integer.MAX_VALUE;
+        if(n <= 3)
+        return 0;
+        for(int i=1; i<=4; i++){
+            min = Math.min(min, Math.abs(nums[n-i] - nums[4-i]));
+        }
+        return min;
+    }
+}
