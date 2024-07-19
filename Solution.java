f9af3382100edb8636da3892954891a65d439d67
NikitaGunjal12 <nikitagunjal.rmdstic.comp@gmail.com>
2024-07-19 22:14:56 +0530

    new msg

--- Solution.java
+++ Solution.java
@@ -12,6 +12,9 @@ In the third move, change 5 to 3. nums becomes [3,3,3,3].
 After performing 3 moves, the difference between the minimum and maximum is 3 - 3 = 0 */
 
 package Practice_Que;
+
+import java.util.Arrays;
+
 class Solution {
     public int minDifference(int[] nums) {
         
@@ -25,3 +28,32 @@ class Solution {
         return min;
     }
 }
+
+//BINARY SEARCH
+/*public class BinarySearch {
+    public static int BinarySearch(int arr[], int key){
+        int start=0;
+        int end = arr.length-1;
+        while(start<=end){
+            int mid = (start + end)/2;
+        
+            if(arr[mid]==key){
+                return mid;
+            }if(arr[mid]<key){
+                start = mid+1;
+            }else{
+                end = mid-1;
+            }
+        }
+        return -1;
+
+    }
+    public static void main(String[]args){
+        int arr[] = {1,2,3,4,5,6,7,8,9};
+        int key =7;
+        System.out.println(BinarySearch(arr, key));
+    }
+}*/
+    
+
+
