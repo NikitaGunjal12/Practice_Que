2b1ed2e357cb592f621399e107e4843d2dd47be5
NikitaGunjal12 <nikitagunjal.rmdstic.comp@gmail.com>
2024-07-20 22:29:54 +0530

    new ex

--- Solution.java
+++ Solution.java
@@ -132,8 +132,67 @@ Output: [[3,0],
         
     }
 }
-
 */
+
+/*Example 4: Delete Node and Return Forest
+ Input: root = [1,2,3,4,5,6,7], to_delete = [3,5]
+Output: [[1,2,null,4],[6],[7]]
+Example 2:
+
+Input: root = [1,2,4,null,3], to_delete = [3]
+Output: [[1,2,4]]
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ 
+class Solution {
+    public TreeNode dfs(TreeNode root, HashSet<Integer> to_delete, List<TreeNode> forest){
+        if(root==null){
+            return root;
+        }
+        root.left = dfs(root.left, to_delete,forest);
+        root.right = dfs(root.right, to_delete,forest);
+        //process the root /node
+        if(!to_delete.contains(root.val)){
+            return root;
+        }
+        //delete this node
+        if(root.left!=null){
+            forest.add(root.left);
+        }
+        if(root.right!=null){
+            forest.add(root.right);
+        }
+        root.left=null;
+        root.right=null;
+        return null;
+    }
+    public List<TreeNode> delNodes(TreeNode root, int[] to_delete) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int to_be_del : to_delete){
+            set.add(to_be_del);
+        }
+        List<TreeNode> forest = new ArrayList<>();
+        root = dfs(root,set,forest);
+        if(root!=null){
+            forest.add(root);
+        }
+        return forest;
+     }
+}
+ */
     
 
 
