7a842a85712146dbcc8b225cc00b923c0dc099fc
NikitaGunjal12 <nikitagunjal.rmdstic.comp@gmail.com>
2024-07-20 22:25:27 +0530

    new Program

--- Solution.java
+++ Solution.java
@@ -102,6 +102,38 @@ class Solution {
 }
 
  */
+
+/*
+ Example 3:find valid matrix given row and column sums.
+Input: rowSum = [3,8], colSum = [4,7]
+Output: [[3,0],
+         [1,7]]
+
+    class Solution {
+    public int[][] restoreMatrix(int[] rowSum, int[] colSum) {
+        int rows = rowSum.length;
+        int cols = colSum.length;
+        int r=0;
+        int c=0;
+        int resMatrix[][] = new int [rows][cols];
+        while(r<rows && c<cols){
+            int minVal = Math.min(rowSum[r],colSum[c]);
+            resMatrix[r][c] = minVal;
+            rowSum[r]-=minVal;
+            colSum[c]-=minVal;
+            if(rowSum[r]==0){
+                r++;
+
+            }else{
+                c++;
+            }
+        }
+        return resMatrix;
+        
+    }
+}
+
+*/
     
 
 
