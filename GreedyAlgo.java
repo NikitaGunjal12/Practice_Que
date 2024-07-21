29618782fa488005d38841e169eb6f40fbc9ec8c
NikitaGunjal12 <nikitagunjal.rmdstic.comp@gmail.com>
2024-07-21 23:15:18 +0530

    greetyAlgo

--- GreedyAlgo.java
+++ GreedyAlgo.java
@@ -113,6 +113,73 @@ public class Indiancoins {
 }
  */
 
+ //Example 5:
+
+/*
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Knapsackpr {
+    public static void main(String[]args){
+        int val[] = {60,100,120};
+        int weight[]={10,20,30};
+        int w=50;
+
+        double ratio[][] = new double[val.length][2];//design TwoD array
+        //0th col=> idx; 1st col=> ratio
+
+        for(int i=0; i<val.length;i++){
+            ratio[i][0] = i;
+            ratio[i][1] = val[i]/(double)weight[i];//calculate ratio
+        }
+
+        //ascending order
+        Arrays.sort(ratio, Comparator.comparingDouble(o-> o[1]));
+
+        int capacity =w;
+        int finalVal = 0;
+        for(int i=ratio.length-1; i>=0; i--){
+            int idx = (int) ratio[i][0];
+            if(capacity>=weight[idx]){
+                finalVal+=val[idx];
+                capacity -= weight[idx];
+            }else{
+                finalVal+=(ratio[i][1] * capacity);
+                capacity =0;
+                break;
+            }
+        }
+        System.out.println("final value=" +finalVal);
+    }
+    
+}
+//Example 6:
+/*
+import java.util.*;
+
+public class Maxlengthchailpair {
+    public static void main(String[]args){
+        int pairs[][] ={{5,24}, {39, 60}, {5,28},{27,40},{50,90}};
+        Arrays.sort(pairs, Comparator.comparingDouble(o -> o[1]));
+        int chainLen =1;
+        int chainEnd = pairs[0][1];
+
+        for(int i=1; i<pairs.length; i++){
+            if(pairs[i][0] >chainEnd){
+                chainLen++;
+                chainEnd= pairs[i][1];
+            }
+        }
+        System.out.println("max length of chain = " + chainLen);
+    }
+    
+}
+
+ 
+ */
+ 
+ 
+
 
     
 
